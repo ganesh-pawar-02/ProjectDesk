b17990e5b49ce43e85f9769d501c3ef02da5c042
ganesh-pawar-02 <ganeshhpawar2002@gmail.com>
2024-11-30 00:11:15 +0530

    Project Added

---
+++ WebByDal/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebByDal.DAL;
+using WebByDal.Models;
+
+namespace WebByDal.Controllers
+{
+    public class HomeController : Controller
+    {   
+        StudentDal stdal=new StudentDal();
+        public IActionResult Index()
+        {
+           List<Student>List = stdal.GetStudents().ToList();
+            return View("Index",List);
+        }
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        public ActionResult AfterCreate(Student studetToAdd)
+        {
+            int rows=stdal.AddStudent(studetToAdd);
+            return Redirect("/Home/Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            Student student= stdal.GetStudent(id);
+            return View("Edit",student);
+        }
+        public IActionResult AfterEdit(Student s)
+        {
+            int rows = stdal.UpdateStudent(s);
+            return Redirect("/Home/Index");
+        }
+        public IActionResult Delete(int id) 
+        {
+            int row=stdal.DeleteStudent(id);
+            return Redirect("/Home/Index");
+        }
+
+    }
+}
---
+++ WebByDal/DAL/StudentDal.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Data.SqlClient;
+using WebByDal.Models;
+
+namespace WebByDal.DAL
+{
+    public class StudentDal
+    {
+        string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=College;Integrated Security=True;Pooling=False;Encrypt=False;Trust Server Certificate=False";
+
+        public List<Student> GetStudents()
+        {
+            string qry = "select * from Student";
+
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(qry, conn);
+
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Student> list = new List<Student>();
+            while (reader.Read())
+            {
+                Student student = new Student();
+                student.Id = Convert.ToInt32(reader["Id"]);
+                student.Name = reader["Name"].ToString();
+                student.Project = reader["Project"].ToString();
+                list.Add(student);
+
+            }
+            conn.Close();
+            return list;
+
+        }
+        public int AddStudent(Student student)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            string qry = "insert into Student (Name,Project) Values ('{0}','{1}')";
+            string qryTxt = string.Format(qry, student.Name, student.Project);
+
+            SqlCommand cmd = new SqlCommand(qryTxt, conn);
+            conn.Open();
+            int rows = cmd.ExecuteNonQuery();
+            conn.Close();
+            return rows;
+
+        }
+        public Student GetStudent(int id)
+        {
+            List<Student> list = GetStudents();
+            Student foundStudent = (from student in list
+                                    where student.Id == id
+                                    select student).First();
+            return foundStudent;
+        }
+        public int UpdateStudent(Student s)
+        {
+            SqlConnection con = new SqlConnection(connString);
+            string qry = "update Student set Name= '{1}', Project='{2}' where Id= {0}";
+            string qryTxt=string.Format(qry, s.Id,s.Name,s.Project);
+            SqlCommand cmd = new SqlCommand(qryTxt, con);
+            con.Open();
+            int rows = cmd.ExecuteNonQuery();
+            con.Close();
+            return rows;
+        }
+        public int DeleteStudent(int id)
+        {
+            SqlConnection con = new SqlConnection(connString);
+            string qry = "delete from Student where Id= {0}";
+            string aryTxt=string.Format(qry, id);
+            con.Open();
+            SqlCommand cmd=new SqlCommand(aryTxt, con);
+            int rows=cmd.ExecuteNonQuery();
+            con.Close();
+            return rows;
+
+        }
+    }
+}
